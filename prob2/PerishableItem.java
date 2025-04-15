d0711f1937807754c79826517da7913128695d71
JaydenThompsonJJT <156786173+JaydenThompsonJJT@users.noreply.github.com>
2025-04-15 13:50:27 -0400

    Add files via upload

---
+++ prob2/PerishableItem.java
@@ -0,0 +1,27 @@
+// Jayden Thompson - PerishableItem
+package prob2;
+
+import Item;
+import java.util.Random;
+
+public class PerishableItem extends Item {
+	private int expireInDays;
+	
+	public PerishableItem(int id, String name) {
+		super(id, name);
+        this.expireInDays = new Random().nextInt(101);
+	}
+	
+	@Override
+	public boolean isPerishable() {
+		return true;
+	}
+	
+	public int getExpireInDays() {
+		return expireInDays;
+	}
+	
+	public boolean isExpired(int currentDay) {
+		return expireInDays - currentDay <= 0;
+	}
+}
